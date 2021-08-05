8ee4c809acddc90f27f20fe1c854da2aa635ba5d
Stanley Salvatierra <s.salvatierra.bo@gmail.com>
2021-08-04 22:19:00 -0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <iframe src="https://www.sicoes.gob.bo/portal/contrataciones/ficha/fichaProceso.php?e=1&cp=21-0417-03-1154024-1-1"
+      title="iframe Example 1" width="400" height="300">
+    </iframe>
       </header>
     </div>
   );
